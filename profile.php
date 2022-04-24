200165aec92e6483bebb3ddaa48b5da1cdb34e4f
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-04-25 10:43:11 +1200

    Update profile.php
    edit some basic layout and details

--- profile.php
+++ profile.php
@@ -1,89 +1,120 @@
 <?php
- session_start();
- include('access.php');
- include('information.php');
+session_start();
+include('access.php');
+include('information.php');
 ?>
 
 <!DOCTYPE htmlPUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
-  <head>
-    <link rel="stylesheet" href="style.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="utilities.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <title>Login Page</title>
-  </head>
-  <body>
-    <!--header bar-->
-    <div class="navbar">
-      <div class="container flex">
-        <h1 class="logo">IFitness</h1>
-        <nav>
-            <ul>
-              <li><a href="index.php">Home</a></li>
-              <li><a href="shop.php">Shop</a></li>
-              </ul>
-            </nav>
-            <nav>
-              <ul>  
-                <li><input type="text" placeholder="Search..."></li>
-                <li><button type="submit"><i class="fa fa-search"></i></button></li>      
-              </ul>
-            </nav>
-            <nav>
-            <ul>
-              <li><a href="cart.php">Cart</a></li>
-              <?php if ($isLoggedIn): ?>
-                <li><a href="?profile=profile">welcome <?=$_SESSION["_reg"]?></a></li>
-                <li><a href="?logout=logout">Logout</a></li>
 
-                <?php else: ?>
-              <li><a href="login.php">Login</a></li>
-              <li><a href="register.php">Register</a></li>
+<head>
+  <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+  <link rel="stylesheet" href="utilities.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <title>Profile Page</title>
+</head>
 
-              <?php endif; ?>
-            </ul>
-          </nav>
+<body>
+  <!--header bar-->
+  <div class="navbar">
+    <div class="container flex">
+      <h1 class="logo">IFitness</h1>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="shop.php">Shop</a></li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li><input type="text" placeholder="Search..."></li>
+          <li><button type="submit"><i class="fa fa-search"></i></button></li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li><a href="cart.php">Cart</a></li>
+          <?php if ($isLoggedIn) : ?>
+            <li><a href="?profile=profile">welcome <?= $_SESSION["_reg"] ?></a></li>
+            <li><a href="?logout=logout">Logout</a></li>
+
+          <?php else : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+
+          <?php endif; ?>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <!--center part -->
+  <div class="center">
+    <div class="login-form">
+      <h2>User Profile</h2>
+      <p>
+      </p>
+      <form>
+        <div class="form-control">
+          <label>Full Name:</label>
+          <input type="text" value="<?php echo $f_name; ?> <?php echo $l_name; ?>" name="firstname" id="firstname" readonly/>
         </div>
+        <div class="form-control">
+        <label>Email:</label>
+          <input type="text" value="<?php echo $email; ?>" name="email" id="email" readonly/>
+        </div>
+        <div class="form-control">
+        <label>Username:</label>
+          <input type="text" value="<?php echo $u_name; ?>" name="username" id="username" readonly/>
+        </div>
+        <div class="form-control">
+        <label>Security Question:</label>
+          <input type="text" value="<?php echo $question; ?>" name="question" id="question" readonly/>
+        </div>
+        <div class="form-control">
+          <label>Security Answer:</label>
+          <input type="password" value="<?php echo $answer; ?>" name="answer" id="myInput" readonly/>
+          <input type="checkbox" onclick="myFunction()">Show Answer
+        </div>
+    </div>
+  </div>
+
+  <!--Footer bar-->
+  <footer class="footer bg-primary py-3">
+    <div class="container grid grid-3">
+      <!--Social media parts-->
+      <div class="social">
+        <a href="#"><i class="fab fa-github fa-2x"></i></a>
+        <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+        <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
       </div>
-      <!--center part -->
+      <div>
+        <ul>
+          <li>
+            Copyright &copy; IFitness. All Rights reserved. 2022
+          </li>
+        </ul>
+      </div>
+      <nav>
+        <!--pages for like others, more about us etc.-->
+        <ul>
+          <li><a href="#"># links 1 </a></li>
+          <li><a href="#"># links 2</a></li>
+          <li><a href="#"># links 3</a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+  <script>
+    function myFunction() {
+      var x = document.getElementById("myInput");
+      if (x.type === "password") {
+        x.type = "text";
+      } else {
+        x.type = "password";
+      }
+    }
+  </script>
+</body>
 
-      <table>
-        <tr><td>First name:</td><td><?php echo $f_name;?></td></tr>
-        <tr><td>Last name:</td><td><?php echo $l_name;?></td></tr>
-        <tr><td>Email:</td><td><?php echo $email;?></td></tr>
-        <tr><td>username:</td><td><?php echo $u_name;?></td></tr>
-        <tr><td>Password:</td><td><?php echo $p_word;?></td></tr>
-        <tr><td>question:</td><td><?php echo $question;?></td></tr>
-        <tr><td>answer:</td><td><?php echo $answer;?></td></tr>
-
-      </table>
-
-            <!--Footer bar-->    
-            <footer class="footer nothing bg-primary py-3">
-        <div class="container grid grid-3">
-          <!--Social media parts-->
-          <div class="social">
-            <a href="#"><i class="fab fa-github fa-2x"></i></a>
-            <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
-            <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
-            <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-        </div>
-          <div>
-                <ul>
-                  <li>
-                    Copyright &copy; IFitness. All Rights reserved. 2022
-                  </li>
-                </ul>
-                </div>
-            <nav>
-              <!--pages for like others, more about us etc.-->
-                <ul>
-                    <li><a href="#"># links 1 </a></li>
-                    <li><a href="#"># links 2</a></li>
-                    <li><a href="#"># links 3</a></li>
-                </ul>
-            </nav>
-        </div>
-    </footer>
-  </body>
 </html>
