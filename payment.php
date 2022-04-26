21e4b53fce988c96c2b120b7a3e1fd070c3ed945
cindychen042 <72330380+cindychen042@users.noreply.github.com>
2022-04-26 18:37:31 +1200

    Update payment.php

--- payment.php
+++ payment.php
@@ -58,47 +58,39 @@ include ('acess.php');
       <form action="/action_page.php">
           <div class="col-50">
           <h3>Enter your payment details</h3>
-            <label for="ccnum">Card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
-            
-            
-            
-              <div class="row">
-              <div class="col-50">
-              <label for="expmonth">Exp Month</label>
-            <input type="text" id="expmonth" name="expmonth" placeholder="September">
-            </div>
-                <div class="col-50">
-                <label for="expyear">Exp Year</label>
-                <input type="text" id="expyear" name="expyear" placeholder="2018">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="352">
-              </div>
-            </div>
+            <input type="text" id="ccnum" name="cardnumber" placeholder="Card number            MM/YY/CVC">          
           </div>
 
                
           <div class="col-50">
-            <label for="adr"><i class="fa fa-address-card-o"></i> Street Address</label>
-            <input type="text" id="adr" name="address" placeholder="542 W. 15th Street">
-            <label for="city"><i class="fa fa-institution"></i> City</label>
-            <input type="text" id="city" name="city" placeholder="New York">
-
-            <div class="row">
-              <div class="col-50">
-                <label for="state">State</label>
-                <input type="text" id="state" name="state" placeholder="NY">
-              </div>
-              <div class="col-50">
-                <label for="zip">Zip</label>
-                <input type="text" id="zip" name="zip" placeholder="10001">
-              </div>
-            </div>    
-        </div>
-        <label>
-          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+            <input type="text" id="adr" name="street address" placeholder="Street address">
+            <input type="text" id="address" name="address" placeholder="Apt,unit,suite,etc.">
+            </div>
+        <div class = "row">
+           <div class = "col-50">
+             <select name = "Country">
+               <option value = "Afghanistan">Afghanistan</option>
+               <option value = "Bahrain">Bahrain</option>
+               <option value = "Cambodia">Cambodia</option>
+                <option value = "Denmark">Denmark</option>
+               <option value = "Finland">Finland</option>
+                <option value = "Japan">Japan</option>
+               <option value = "Mexico">Mexico</option>
+                <option value = "Netherlands">Netherlands</option>
+               <option value = "New Zealand">New Zealand</option>
+                <option value = "United States">United States</option>
+               <Select>
+            </div>  
+               <div class = "col-50">  
+                  <input type="text" id="city" name="city" placeholder="City"> 
+               </div>
+               <div>
+               
+                 <div class ="col-50">
+                  <input type="text" id="state" name="state" placeholder="State"> 
+                 <input type = "text" id = "zip" name = "zip" placeholder="Zip code">
+                 </div>
+                 
         </label>
         <input type="submit" value="Continue to checkout" class="btn">
       </form>
