ca86b8e34691a27c8a5177dd09a2640540903d4e
Allenegra <101927886+Allenegra@users.noreply.github.com>
2022-04-28 07:55:32 +0800

    add file
    add file

---
+++ add2cart.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include('access.php');
+
+$product_id = $_GET['product_id'];
+
+$product_query = "SELECT * FROM products WHERE id = '$product_id';";
+$product_results = mysqli_query($conn, $product_query);
+while ($row = mysqli_fetch_assoc($product_results))
+{
+  $u_name=$_SESSION['_reg'];
+  $name=$row['name'];
+  $price=$row['price'];
+  $image=$row['image'];
+  $quantity=$_GET['quantity'];
+  $sql = "INSERT INTO cart (u_name, name, price, image, quantity) VALUES ('$u_name', '$name', '$price', '$image', '$quantity');";
+  mysqli_query($conn, $sql);
+  echo "<script>window.open('cart.php','_self')</script>";
+  break;
+}
---
+++ delete_cart.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+include('access.php');
+
+$id = $_GET['id'];
+$u_name=$_SESSION['_reg'];
+
+if ($id == 'all') {
+    $sql = "DELETE FROM cart WHERE u_name = '$u_name';";
+} else {
+    $sql = "DELETE FROM cart WHERE id = '$id' and u_name = '$u_name';";
+}
+
+mysqli_query($conn, $sql);
+echo "<script>window.open('cart.php','_self')</script>";
