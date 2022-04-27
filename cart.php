919ab74ebc77a0f52331a9c19d5c822ec559d860
Allenegra <101927886+Allenegra@users.noreply.github.com>
2022-04-28 07:54:31 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 include('access.php');
+
+$u_name = $_SESSION['_reg'];
+
+$cart_query = "SELECT * FROM cart WHERE u_name = '$u_name';";
+$cart_results = mysqli_query($conn, $cart_query);
 ?>
 
 <!DOCTYPE htmlPUBLIC "-//W3C//DTD HTML 4.01//EN">
@@ -43,6 +48,65 @@ include('access.php');
     </ul>
   </nav>
   <!-- Center screen-->
+  <div class="shopping-cast">
+    <p class="shopping-cart-header">
+      SHOPPING CART
+    </p>
+    <table style="border: 1px solid black;">
+      <thead>
+        <th>image</th>
+        <th>name</th>
+        <th>price</th>
+        <th>quantity</th>
+        <th>total price</th>
+        <th>action</th>
+      </thead>
+      <?php
+
+      $total_price = 0;
+
+      while ($row = mysqli_fetch_assoc($cart_results)) {
+        $price = $row['quantity'] * $row['price'];
+        $total_price = $total_price + $price;
+      ?>
+        <tr>
+          <td style="white-space: nowrap;"><img src="<?php echo str_replace('pd', '', str_replace('../', '/', $row['image'])) ?>" /></td>
+          <td width="99%"" style=" font-weight: bold;"><?php echo $row['name'] ?></td>
+          <td style="white-space: nowrap;">$<?php echo $row['price'] ?>/-</td>
+          <td style="white-space: nowrap;">
+            <form action="/update_cart.php" method="get">
+              <input type="number" class="quantity-input" value="<?php echo $row['quantity'] ?>" name="quantity">
+              <input type="number" value="<?php echo $row['id'] ?>" name="id" hidden>
+              <button class="quantity-update-btn">Update</button>
+            </form>
+          </td>
+          <td style="white-space: nowrap;">$<?php echo $row['quantity'] * $row['price'] ?>/-</td>
+          <td style="white-space: nowrap;"">
+            <button class=" remove-btn" onclick="location.href='/delete_cart.php?id=<?php echo $row['id'] ?>';">Remove</button>
+          </td>
+        </tr>
+      <?php
+      }
+      ?>
+      <tr>
+        <td></td>
+        <td>grand total:</td>
+        <td></td>
+        <td></td>
+        <td>$<?php echo $total_price ?>/-</td>
+        <td>
+          <button class=" remove-btn" onclick="location.href='/delete_cart.php?id=all';">Remove All</button>
+        </td>
+      </tr>
+    </table>
+    <div style="width: 20%;margin: auto;">
+      <button class="checkout-btn">Proceed To Checkout</button>
+    </div>
+  </div>
+
+
+
+
 
   <!--Footer bar-->
   <footer class="footer nothing bg-primary py-3">
