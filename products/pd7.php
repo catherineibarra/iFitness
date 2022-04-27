a8eff34e86b3a12acf33696a994ea5429d2b8ff3
MissChenduo <zdt5500@autuni.ac.nz>
2022-04-27 19:03:33 +1200

    fixed the product page errors
    fixed the product page errors

--- products/pd7.php
+++ products/pd7.php
@@ -92,8 +92,7 @@ include('./location.php');
             </div>
 
           </div>
-          <a class="prev  fa-solid fa-arrow-left-long" onclick="plusSlides(-1)">
-            << /a>
+          <a class="prev  fa-solid fa-arrow-left-long" onclick="plusSlides(-1)"><</a>
               <a class="next  fa-solid fa-arrow-right-long" onclick="plusSlides(1)">></a>
         </div>
 
