ca21565f5307cbdd2cc21d3e132149bb87df29f5
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-04-30 10:08:55 +1200

    updating view more
    -fixed hover icons not working
    make use of view more bring back to products

--- products/pd7.php
+++ products/pd7.php
@@ -14,6 +14,7 @@ include('./location.php');
   <link rel="stylesheet" href="../css/utilities.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>
   <title> Product Seven | iFitness </title>
 </head>
 
@@ -147,7 +148,7 @@ include('./location.php');
   <section class="section featured">
     <div class="top container">
       <h1>Related Products</h1>
-      <a href="#" class="view-more">View more</a>
+      <a href="../shop.php" class="view-more">View more</a>
     </div>
     <div class="product-center container">
       <div class="product-item">
