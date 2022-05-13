ab728b342406f201e577443334096df956d80c93
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-05-14 09:24:07 +1200

    update profile
    fixing header and footer to match all current pages

--- update.php
+++ update.php
@@ -8,45 +8,53 @@ include('information.php');
 <html>
 
 <head>
+  <title> Home | iFitness </title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA_Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- IMPORT CSS -->
   <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
   <link rel="stylesheet" href="css/utilities.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-  <title>Profile Page</title>
+  <link rel="stylesheet" href="css/awesome.css">
+
+  <!-- IMPORT FONTS -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+  <!-- IMPORT VECTORS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous">
 </head>
 
 <body>
   <!--header bar-->
-  <div class="navbar">
-    <div class="container flex">
-    <li class="logo"><img src="./images/logo3.png"></li>
-      <nav>
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="shop.php">Shop</a></li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li><input type="text" value="Search..."></li>
-          <li><button type="submit"><i class="fa fa-search"></i></button></li>
-        </ul>
-      </nav>
-      <nav>
-        <ul>
-          <li><a href="cart.php">My Cart</a></li>
-          <?php if ($isLoggedIn) : ?>
-            <li><a href="?profile=profile">My Account</a></li>
-            <li><a href="?logout=logout">Logout</a></li>
+  <nav>
+    <ul>
+      <li class="logo"><img src="./images/logo3.png"></li>
+      <div class="navbar">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="shop.php">Shop</a></li>
 
-          <?php else : ?>
-            <li><a href="login.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
+        <li><a href="cart.php">My Cart</a></li>
+        <?php if ($isLoggedIn) : ?>
+          <li><a href="?profile=profile">My Account</a></li>
+          <li><a href="?logout=logout">Logout</a></li>
 
-          <?php endif; ?>
-        </ul>
-      </nav>
-    </div>
-  </div>
+        <?php else : ?>
+          <li><a href="login.php">Login</a></li>
+          <li class="reg-nav"><a href="register.php">Register</a></li>
+
+        <?php endif; ?>
+
+      </div>
+      <li class="search-icon">
+        <input type="search" placeholder="Search">
+        <button type="submit"><i class="fa fa-search"></i></button>
+      </li>
+    </ul>
+  </nav>
+
   <!--center part -->
   <div class="center">
     <div class="login-form">
@@ -95,32 +103,25 @@ include('information.php');
   </div>
 
   <!--Footer bar-->
-  <footer class="footer bg-primary py-3">
-    <div class="container grid grid-3">
-      <!--Social media parts-->
+  <footer class="nothing">
+    <ul>
       <div class="social">
-        <a href="#"><i class="fab fa-github fa-2x"></i></a>
-        <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
-        <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
-        <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+        <a href="https://github.com/catherineibarra/iFitness"><i class="fab fa-github fa-2x"></i></a>
+        <a href="not-found.html"><i class="fab fa-facebook fa-2x"></i></a>
+        <a href="not-found.html"><i class="fab fa-instagram fa-2x"></i></a>
+        <a href="not-found.html"><i class="fab fa-twitter fa-2x"></i></a>
       </div>
-      <div>
-        <ul>
-          <li>
-            Copyright &copy; IFitness. All Rights reserved. 2022
-          </li>
-        </ul>
+
+      <li>&copy; 2022 iFitness. All Rights Reserved.</li>
+
+      <div class="navbar">
+        <li><a href="not-found.html">About Us </a></li>
+        <li><a href="not-found.html">Terms and Policy</a></li>
       </div>
-      <nav>
-        <!--pages for like others, more about us etc.-->
-        <ul>
-          <li><a href="#"># links 1 </a></li>
-          <li><a href="#"># links 2</a></li>
-          <li><a href="#"># links 3</a></li>
-        </ul>
-      </nav>
-    </div>
+    </ul>
   </footer>
+
+  <!--Script -->
   <script>
     function myFunction() {
       var x = document.getElementById("myInput");
