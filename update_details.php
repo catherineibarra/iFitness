66a5e9671d6772a840b1c93bddcbd05fbaa98f91
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-05-14 09:24:26 +1200

    update details
    change from html page to php file

--- update_details.php
+++ update_details.php
@@ -1,48 +1,6 @@
 <?php
  include('access.php');
 ?>
-<!DOCTYPE htmlPUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html>
-  <head>
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="css/utilities.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
-    <title>Login Page</title>
-  </head>
-  <body>
-    <!--header bar-->
-    <div class="navbar">
-      <div class="container flex">
-      <li class="logo"><img src="./images/logo3.png"></li>
-        <nav>
-            <ul>
-              <li><a href="index.php">Home</a></li>
-              <li><a href="shop.php">Shop</a></li>
-              </ul>
-            </nav>
-            <nav>
-              <ul>  
-                <li><input type="text" placeholder="Search..."></li>
-                <li><button type="submit"><i class="fa fa-search"></i></button></li>      
-              </ul>
-            </nav>
-            <nav>
-            <ul>
-              <li><a href="cart.php">My Cart</a></li>
-              <?php if ($isLoggedIn): ?>
-                <li><a href="?profile=profile">My Account</a></li>
-                <li><a href="?logout=logout">Logout</a></li>
-
-                <?php else: ?>
-              <li><a href="login.php">Login</a></li>
-              <li><a href="register.php">Register</a></li>
-
-              <?php endif; ?>
-            </ul>
-          </nav>
-        </div>
-      </div>
-
 <?php
 
  $u_firstname = $_POST["u_firstname"];
@@ -108,32 +66,3 @@ $encrypt_code = md5($u_username);
     }
 
 ?>
- <!--Footer bar-->    
- <footer class="footer nothing bg-primary py-3">
-        <div class="container grid grid-3">
-          <!--Social media parts-->
-          <div class="social">
-            <a href="#"><i class="fab fa-github fa-2x"></i></a>
-            <a href="#"><i class="fab fa-facebook fa-2x"></i></a>
-            <a href="#"><i class="fab fa-instagram fa-2x"></i></a>
-            <a href="#"><i class="fab fa-twitter fa-2x"></i></a>
-        </div>
-          <div>
-                <ul>
-                  <li>
-                    Copyright &copy; IFitness. All Rights reserved. 2022
-                  </li>
-                </ul>
-                </div>
-            <nav>
-              <!--pages for like others, more about us etc.-->
-                <ul>
-                    <li><a href="#"># links 1 </a></li>
-                    <li><a href="#"># links 2</a></li>
-                    <li><a href="#"># links 3</a></li>
-                </ul>
-            </nav>
-        </div>
-    </footer>
-  </body>
-</html>
