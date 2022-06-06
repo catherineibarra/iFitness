b5561ff017ec0cb898ff5066ddc3ce30714c4721
yeoj1092 <83740592+yeoj1092@users.noreply.github.com>
2022-06-06 19:31:20 +1200

    updated access page
    updated

--- access.php
+++ access.php
@@ -76,43 +76,44 @@ mysqli_query($conn, $create_ordercart_table);
 $products_table_exists = mysqli_query($conn, "SELECT * FROM products;");
 if(!$products_table_exists){
   //query for creating table
-  $create_product_table = "CREATE TABLE IF NOT EXISTS `products` (`id` int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` varchar(100) NOT NULL,`price` varchar(100) NOT NULL,`image` varchar(100) NOT NULL);";
+
+  $create_product_table = "CREATE TABLE IF NOT EXISTS `products` (`id` int(100) NOT NULL AUTO_INCREMENT PRIMARY KEY,`name` varchar(100) NOT NULL,`price` varchar(100) NOT NULL,`image` varchar(100) NOT NULL,`links` varchar(100) NOT NULL);";
   mysqli_query($conn, $create_product_table);
 
   //insert information into product table
-  $create_product =   "INSERT INTO `products` (`id`, `name`, `price`, `image`) VALUES
-  (null, 'Ghost 100% Whey Protein 2lb', '60.21', './images/pd1.jpg'), 
-  (null, 'Musashi High Protein Chocolate 2kg', '76.59', './images/pd2.jpg'),
-  (null, 'Muscle Matrix - 60 vcaps (25mg per capsule)', '109.00', './images/pd3.jpg'),
-  (null, 'Scream Supps PINHEAD 1 Serve Sample', '59.99', './images/pd4.jpg'),
-  (null, 'NIVERSAL ANIMAL ENERGY 60 CAPSUL', '29.00', './images/pd5.jpg'),
-  (null, 'PREMIUM HERBAL SUPPLEMENTS', '39.00', '../images/pd6.jpg'),
-  (null, 'Cellucor C4 Original ID 60 Serves', '68.90', './images/pd7.jpg'),
-  (null, 'Rich Piana 5% Nutrition Freak Show', '78.90', './images/pd8.jpg'),
-  (null, 'Muscle Builder 90 capsules', '77.00', './images/pd9.jpg'),
-  (null, 'EHP Labs OxyShred Thermogenic Fat Burner', '79.89', './images/pd140.jpg'), ## 10 items
+  $create_product =   "INSERT INTO `products` (`id`, `name`, `price`, `image`, `links`) VALUES
+  (null, 'Ghost 100% Whey Protein 2lb', '60.21', './images/pd1.jpg','./products/pd1.php'), 
+  (null, 'Musashi High Protein Chocolate 2kg', '76.59', './images/pd2.jpg','./products/pd2.php'),
+  (null, 'Muscle Matrix - 60 vcaps (25mg per capsule)', '109.00', './images/pd3.jpg','./products/pd3.php'),
+  (null, 'Scream Supps PINHEAD 1 Serve Sample', '59.99', './images/pd4.jpg','./products/pd4.php'),
+  (null, 'NIVERSAL ANIMAL ENERGY 60 CAPSUL', '29.00', './images/pd5.jpg','./products/pd5.php'),
+  (null, 'PREMIUM HERBAL SUPPLEMENTS', '39.00', '../images/pd6.jpg','./products/pd6.php'),
+  (null, 'Cellucor C4 Original ID 60 Serves', '68.90', './images/pd7.jpg','./products/pd7.php'),
+  (null, 'Rich Piana 5% Nutrition Freak Show', '78.90', './images/pd8.jpg','./products/pd8.php'),
+  (null, 'Muscle Builder 90 capsules', '77.00', './images/pd9.jpg','./products/pd9.php'),
+  (null, 'EHP Labs OxyShred Thermogenic Fat Burner', '79.89', './images/pd140.jpg','./products/pd10.php'), ## 10 items
 
-  (null, 'Fusion Push-up Workout Set', '69.99', './images/pd111.jpg'),
-  (null, 'WalkingPad Foldable Treadmill', '1099.00', './images/pd120.jpg'),
-  (null, 'Two Person Station Multi Gym/Station With Smith Machine', '2037.00', './images/pd102.png'),
-  (null, 'Thigh Toner Workout Equipment For Women', '24.97', './images/pd130.jpg'),
-  (null, 'Sports Suits For Women', '169.99', './images/pd150.jpg'),
-  (null, 'Gym Long Sleeve Top', '129.99', './images/pd162.jpg'),
-  (null, 'Sport Suit Workout Sportswear', '102.00', './images/pd170.jpg'),
-  (null, 'Dark Grey Marl For Men', '109.00', './images/pd180.jpg'),
-  (null, 'Charcoal Marl For Men', '179.99', './images/pd190.jpg'),
-  (null, 'MP Men`s Linear Mark Graphic Training 1/4 Zip - Ice Blue', '200', './images/pd201.jpg'), ## 20 items
+  (null, 'Fusion Push-up Workout Set', '69.99', './images/pd111.jpg','./products/pd11.php'),
+  (null, 'WalkingPad Foldable Treadmill', '1099.00', './images/pd120.jpg','./products/pd12.php'),
+  (null, 'Two Person Station Multi Gym/Station With Smith Machine', '2037.00', './images/pd102.png','./products/pd13.php'),
+  (null, 'Thigh Toner Workout Equipment For Women', '24.97', './images/pd130.jpg','./products/pd14.php'),
+  (null, 'Sports Suits For Women', '169.99', './images/pd150.jpg','./products/pd15.php'),
+  (null, 'Gym Long Sleeve Top', '129.99', './images/pd162.jpg','./products/pd16.php'),
+  (null, 'Sport Suit Workout Sportswear', '102.00', './images/pd170.jpg','./products/pd17.php'),
+  (null, 'Dark Grey Marl For Men', '109.00', './images/pd180.jpg','./products/pd18.php'),
+  (null, 'Charcoal Marl For Men', '179.99', './images/pd190.jpg','./products/pd19.php'),
+  (null, 'MP Men`s Linear Mark Graphic Training 1/4 Zip - Ice Blue', '200', './images/pd201.jpg','./products/pd20.php'), ## 20 items
 
-  (null, 'Rockit Dumbbell Rack', '702', './images/pd210.jpg'),
-  (null, 'Home Gym Power Tower Fitness Workout Equipment Height Adjustable Chin Pull Up Bar Dip Station Weight Bench', '319.97', './images/pd220.jpg'),
-  (null, 'Muliti Gym 3 Station Jx930 158LBS (72KG) Stack Weights JX Fitness Home GYM', '1299', './images/pd230.jpg'),
-  (null, 'Gym Weight Training Exercise Equipment Strength Machine', '369.99', './images/pd240.jpg'),
-  (null, 'Genki Magnetic Cross Trainer Elliptical Trainer Machine Home Gym Equipment with LED Display', '39.88', './images/pd250.jpg'),
-  (null, 'Genki Exercise Bike Spin Stationary Shock Absorbing Home Gym Fitness Equipment Adjustable Magnetic Resistance', '379.97', './images/pd260.jpg'),
-  (null, '7-In-1 Weight Bench Multi-Function Power Station Fitness Gym Equipment', '150.30', './images/pd270.jpg'),
-  (null, 'Centra Magnetic Rowing Machine 8 Level Resistance Exercise Fitness Home Gym', '420.00', './images/pd280.jpg'),
-  (null, 'Push Up Bracket', '35.00', './images/pd290.jpg'),
-  (null, 'Wall Mounted Pull Up Multi Function Home Gym Exercise Fitness Indoor Exercise Equipment Gym House Fitness US STORE', '756.99', './images/pd300.jpg');"; ## 30 items
+  (null, 'Rockit Dumbbell Rack', '702', './images/pd210.jpg','./products/pd21.php'),
+  (null, 'Home Gym Power Tower Fitness Workout Equipment Height Adjustable Chin Pull Up Bar Dip Station Weight Bench', '319.97', './images/pd220.jpg','./products/pd22.php'),
+  (null, 'Muliti Gym 3 Station Jx930 158LBS (72KG) Stack Weights JX Fitness Home GYM', '1299', './images/pd230.jpg','./products/pd23.php'),
+  (null, 'Gym Weight Training Exercise Equipment Strength Machine', '369.99', './images/pd240.jpg','./products/pd24.php'),
+  (null, 'Genki Magnetic Cross Trainer Elliptical Trainer Machine Home Gym Equipment with LED Display', '39.88', './images/pd250.jpg','./products/pd25.php'),
+  (null, 'Genki Exercise Bike Spin Stationary Shock Absorbing Home Gym Fitness Equipment Adjustable Magnetic Resistance', '379.97', './images/pd260.jpg','./products/pd26.php'),
+  (null, '7-In-1 Weight Bench Multi-Function Power Station Fitness Gym Equipment', '150.30', './images/pd270.jpg','./products/pd27.php'),
+  (null, 'Centra Magnetic Rowing Machine 8 Level Resistance Exercise Fitness Home Gym', '420.00', './images/pd280.jpg','./products/pd28.php'),
+  (null, 'Push Up Bracket', '35.00', './images/pd290.jpg','./products/pd29.php'),
+  (null, 'Wall Mounted Pull Up Multi Function Home Gym Exercise Fitness Indoor Exercise Equipment Gym House Fitness US STORE', '756.99', './images/pd300.jpg','./products/pd30.php');"; ## 30 items
 
 //query run
 mysqli_query($conn, $create_product);
