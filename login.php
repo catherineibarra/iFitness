603a0a01549c116f90cb46a832623637acc11778
cindychen042 <72330380+cindychen042@users.noreply.github.com>
2022-06-07 20:56:10 +1200

    Update login.php

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ include('access.php');
       <li>&copy; 2022 iFitness. All Rights Reserved.</li>
 
       <div class="navbar">
-        <li><a href="not-found.html">About Us </a></li>
+        <li><a href="aboutus.html">About Us </a></li>
         <li><a href="not-found.html">Terms and Policy</a></li>
       </div>
     </ul>
