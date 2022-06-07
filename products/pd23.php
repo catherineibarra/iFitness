39e5b19efedde651da08c6c997950bc6ea73999b
cindychen042 <72330380+cindychen042@users.noreply.github.com>
2022-06-07 22:59:52 +1200

    Update pd23.php

--- products/pd23.php
+++ products/pd23.php
@@ -417,7 +417,7 @@ include('./location.php');
       <li>&copy; 2022 iFitness. All Rights Reserved.</li>
 
       <div class="navbar">
-        <li><a href="not-found.html">About Us </a></li>
+        <li><a href="aboutus.php">About Us </a></li>
         <li><a href="not-found.html">Terms and Policy</a></li>
       </div>
     </ul>
