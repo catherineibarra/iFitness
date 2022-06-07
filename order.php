aa02afb7b3fd669768e12585ff5f5696945ec784
cindychen042 <72330380+cindychen042@users.noreply.github.com>
2022-06-07 21:44:45 +1200

    Update order.php

--- order.php
+++ order.php
@@ -126,7 +126,7 @@ $cart_results = mysqli_query($conn, $cart_query);
       <li>&copy; 2022 iFitness. All Rights Reserved.</li>
 
       <div class="navbar">
-        <li><a href="not-found.html">About Us </a></li>
+        <li><a href="aboutus.php">About Us </a></li>
         <li><a href="not-found.html">Terms and Policy</a></li>
       </div>
     </ul>
