b0cfa250df0d7e4835a68ebf0bf8a3eafd1fe929
cindychen042 <72330380+cindychen042@users.noreply.github.com>
2022-06-07 21:44:09 +1200

    Update shop.php

--- shop.php
+++ shop.php
@@ -312,7 +312,7 @@ include('access.php');
       <li>&copy; 2022 iFitness. All Rights Reserved.</li>
 
       <div class="navbar">
-        <li><a href="not-found.html">About Us </a></li>
+        <li><a href="aboutus.php">About Us </a></li>
         <li><a href="not-found.html">Terms and Policy</a></li>
       </div>
     </ul>
